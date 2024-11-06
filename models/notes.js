977fa78a3ed417477ddf47c19f93685e86d86c52
Adham <adhamchan2014@gmail.com>
2024-11-06 17:41:56 +0800

    Created and edited account.js, accounts.json, notesUtils.js and index.js

--- models/notes.js
+++ models/notes.js
@@ -1,11 +1,13 @@
 class Notes {
-    constructor(title, description) {
+    constructor(title, description, userId) {
         this.title = title;
         this.description = description;
         this.priority = ["Extreme Priority", "High Priority", "Medium Priority", "Low Priority"];
         const timestamp = new Date().getTime();
         const random = Math.floor(Math.random() * 1000);
         this.id = timestamp + "" + random.toString().padStart(3, '0');
+        this.userId = userId;
     }
 }
+
 module.exports = { Notes }; 
