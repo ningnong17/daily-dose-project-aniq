19dff379e8823467c3223d6c3ed430553d21ef68
pingjiarong <pingjiarong879@gmail.com>
2024-11-07 16:54:33 +0800

    Created add notes branch and code for adding notes

--- utils/NotesUtils.js
+++ utils/NotesUtils.js
@@ -42,7 +42,9 @@ async function addNotes (req, res){
 
 async function createAccount (req, res){
     try{
-        
+
+    }catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 }
 module.exports = {
