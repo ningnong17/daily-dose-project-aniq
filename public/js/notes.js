a269cdd30f7a219f8421efe6636396e0b3e9e7ab
Adham <adhamchan2014@gmail.com>
2024-11-08 17:50:28 +0800

    Made sure that view on the index.html is able to see the added items

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,10 +1,10 @@
-function addNotes(){
+function addNotes() {
     var response = "";
     var jsonData = new Object();
     jsonData.title = document.getElementById("title").value;
     jsonData.description = document.getElementById("description").value;
     jsonData.priority = document.getElementById("priority").value;
-    if (jsonData.title == "" || jsonData.description == "" || jsonData.priority == ""){
+    if (jsonData.title == "" || jsonData.description == "" || jsonData.priority == "") {
         document.getElementById("message").innerHTML = 'All fields are required!';
         document.getElementById("message").setAttribute("class", "text-danger");
         return;
@@ -12,10 +12,10 @@ function addNotes(){
     var request = new XMLHttpRequest();
     request.open("POST", "/add-notes", true);
     request.setRequestHeader('Content-Type', 'application/json');
-    request.onload = function (){
+    request.onload = function () {
         response = JSON.parse(request.responseText);
         console.log(response)
-        if (response.message == undefined){
+        if (response.message == undefined) {
             document.getElementById("message").innerHTML = 'Added Notes: ' + jsonData.name + '!';
             document.getElementById("message").setAttribute("class", "text-success");
             document.getElementById("title").value = "";
@@ -31,3 +31,32 @@ function addNotes(){
     };
     request.send(JSON.stringify(jsonData));
 }
+
+function viewNotes() {
+    var request = new XMLHttpRequest();
+    request.open('GET', '/view-notes', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function () {
+        var response = JSON.parse(request.responseText);
+        var html = '';
+
+        for (var i = 0; i < response.length; i++) {
+            html += `
+                    <div class="col-md-4 mt-4">
+                    <div class="note-card">
+                        <h3 class="note-title">${response[i].title}</h3>
+                        <p class="note-description">${response[i].description}</p>
+                        <span class="note-priority ${response[i].priority}">
+                            ${response[i].priority.replace("-", " ").toUpperCase()}
+                        </span>
+                        <button class="btn btn-sm btn-danger mt-3" onclick="deleteNote(${response[i].id})">Delete</button>
+                    </div>
+                </div>
+            `;
+        }
+
+        document.getElementById('tableContent').innerHTML = html;
+    };
+    request.send();
+}
+
