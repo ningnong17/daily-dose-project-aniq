d08ef6caac41373de588eb4509d359403af4aaf4
pingjiarong-dvops <pingjiarong879@gmail.com>
2024-11-08 23:25:27 +0800

    Revert "Deleted and only left the files I need for my own branch"

---
+++ public/js/viewNotes.js
@@ -0,0 +1,29 @@
+function viewNotes() {
+    var request = new XMLHttpRequest();
+    request.open('GET', '/view-notes', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function () {
+        var response = JSON.parse(request.responseText);
+        var html = '';
+
+        for (var i = 0; i < response.length; i++) {
+            html += `
+    
+                    <div class="note-card">
+                        <h3 class="note-title">${response[i].title}</h3>
+                        <span class="note-description">${response[i].description}</span>
+                        <span class="note-priority ${response[i].priority}">
+                            ${response[i].priority.replace("-", " ").toUpperCase()}
+                        </span>
+                        <div class="note-buttons">
+                         <button type="button" class="btn btn-sm btn-warning" onclick="editNote('${encodeURIComponent(JSON.stringify(response[i]))}')"><span class="material-icons">edit</span></button>
+                         <button class="btn btn-sm btn-danger" onclick="deleteNotes(${response[i].id})"><span class="material-icons">delete</span></button>
+                         </div>
+                    </div>
+            `;
+        }
+
+        document.getElementById('tableContent').innerHTML = html;
+    };
+    request.send();
+}
