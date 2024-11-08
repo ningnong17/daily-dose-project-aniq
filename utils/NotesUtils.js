46e6fd82213445e277555d83d8ec4e39fa20d0e8
aniq amir <notaniqamir@gmail.com>
2024-11-08 17:56:16 +0800

    delete function being added

--- utils/NotesUtils.js
+++ utils/NotesUtils.js
@@ -86,6 +86,32 @@ async function editNotes(req, res) {
         return res.status(500).json({ message: error.message });
     }
 }
+//delete notes - aniq
+async function deleteNotes(req, res) {
+    try {
+        const id = req.params.id;
+        
+        const allNotes = await readJSON('utils/notes.json');
+        var index = -1;
+        
+        for (var i = 0; i < allNotes.length; i++) {
+            var curcurrNotes = allNotes[i];
+            if (curcurrNotes.id == id)
+                index = i;
+        }
+
+        if (index != -1) {
+            allNotes.splice(index, 1);
+            await fs.writeFile('utils/notes.json', JSON.stringify(allNotes), 'utf8');
+            return res.status(201).json({ message: 'note deleted successfully!' });
+        } else {
+            return res.status(500).json({ message: 'Error occurred, unable to delete!' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
 
 //Create Account 
 // async function createAccount(req, res) {
@@ -97,5 +123,5 @@ async function editNotes(req, res) {
 // }
 
 module.exports = {
-    readJSON, writeJSON, addNotes, viewNotes, editNotes
+    readJSON, writeJSON, addNotes, viewNotes, editNotes, deleteNotes
 }
